a1d28be42a213e3350eea4636f337b237075d1f2
gopal jee <gopaljee023@gmail.com>
2020-07-06 11:19:39 +0530

    How to use ‘path’’s join method

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ var chalk = require('chalk')
 var debug = require('debug')('app')
 var morgan = require('morgan');
 
+const path = require('path');
+
 var app = express();
 
 app.use(morgan("tiny"))
 
 app.get('/',(req,res)=>{
-    res.send('Hello Gopal');
+   // res.send('Hello Gopal');
+    res.sendFile(path.join(__dirname,"views","index.html"));
 });
 
 app.listen(3000, ()=>{
     //debug.log(`listening on port ${chalk.green('3000')}`); //it is working
     debug(`listening on port ${chalk.green('3000')}`);
 });
+
+console.log(path.parse('C:\\path\\dir\\file.txt'));
