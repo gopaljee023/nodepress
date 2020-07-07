c8329daba46c4735a79b0853d0607607e970cafc
gopal <gopaljee023@gmail.com>
2020-07-07 14:42:21 +0530

    what is use 'env" variable through nodemon configuration

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(morgan('tiny'));
 
 // app.get('/',(req,res)=>{
@@ -15,7 +15,7 @@ app.get('/greet', (req, res) => {
   res.send('Hello Gopal');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   // debug.log(`listening on port ${chalk.green('3000')}`); //it is working
-  debug(`listening on port ${chalk.green('3000')}`);
+  debug(`listening on port ${chalk.green(port)}`);
 });
