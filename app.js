ac8790286e4cd8f276ff156bd49182bf6f3d91c9
gopal <gopaljee023@gmail.com>
2020-07-07 14:29:23 +0530

    installed eslint and refactor code.. installed nodemon globally added nodeConfig in packages.json updaed 'start' script to use nodemon

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var express = require('express')
-var chalk = require('chalk')
-var debug = require('debug')('app')
- var morgan = require('morgan');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
-var app = express();
+const app = express();
 
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
 // app.get('/',(req,res)=>{
 //     res.send('Hello Gopal');
 // });
 
-app.get('/greet',(req,res)=>{
-    res.send('Hello Gopal');
+app.get('/greet', (req, res) => {
+  res.send('Hello Gopal');
 });
 
-app.listen(3000, ()=>{
-    //debug.log(`listening on port ${chalk.green('3000')}`); //it is working
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(3000, () => {
+  // debug.log(`listening on port ${chalk.green('3000')}`); //it is working
+  debug(`listening on port ${chalk.green('3000')}`);
 });
