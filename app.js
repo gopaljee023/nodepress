51261b64280746294dc973d5768111bbf7318787
gopal <gopaljee023@gmail.com>
2020-07-07 18:08:37 +0530

    check app.js and index.ejs to understand how to use ejs

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 
-const app = express();
 const port = process.env.PORT || 3000;
+
+const app = express();
+
 app.use(morgan('tiny'));
 
-// app.get('/',(req,res)=>{
-//     res.send('Hello Gopal');
-// });
+app.set('views', './src/view');
+app.set('view engine', 'ejs');
 
-app.get('/greet', (req, res) => {
-  res.send('Hello Gopal');
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Gopal' });
 });
 
 app.listen(port, () => {
